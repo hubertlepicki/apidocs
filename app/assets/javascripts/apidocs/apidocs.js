757d09bd9d06ff0f3530a9e2b4dfa89693a7d234
Vladimir Motsak <vmotsak@gmail.com>
2014-07-16 11:48:38 +0200

    long paths wrap in index

--- app/assets/javascripts/apidocs/apidocs.js
+++ app/assets/javascripts/apidocs/apidocs.js
@@ -1,8 +1,8 @@
 $(function () {
     $('.nav-button').click(function () {
-	window.location.href = this.getAttribute("href") + '&search='+$('#search-input').val()
+        window.location.href = this.getAttribute("href") + '&search=' + $('#search-input').val();
         return false;
     });
     $('#search-input').fastLiveFilter('#search-list');
-	$('#search-input').trigger('change');
+    $('#search-input').trigger('change');
 });
