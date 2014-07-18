6a6ee0458d66a423b3997288f9384e8508930608
Hubert Łępicki <hubert.lepicki@amberbit.com>
2014-07-18 08:05:30 +0200

    moar tests

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -12,4 +12,17 @@ feature 'Homepage', js: true do
 
     page.should have_content("Dummy APP")
   end
+
+  scenario 'should show links to API endpoints' do
+    visit '/apidocs'
+
+    page.should have_link("/api/session")
+    page.should have_link("/api/bar")
+  end
+
+  scenario 'should not show links that do not match regex' do
+    visit '/apidocs'
+
+    page.should_not have_link("/something/else")
+  end
 end
