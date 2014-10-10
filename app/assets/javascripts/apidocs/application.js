d8650776243fed5b64a5e14518eb7345cedf7ca2
Hubert Łępicki <hubert.lepicki@amberbit.com>
2014-10-10 15:17:08 +0200

    properly require assets

--- app/assets/javascripts/apidocs/application.js
+++ app/assets/javascripts/apidocs/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+//= require apidocs/jquery
+//= require apidocs/jquery_ujs
+//= require apidocs/jquery.fastLiveFilter
+//= require apidocs/apidocs
